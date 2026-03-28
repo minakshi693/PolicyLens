cdb8dbafbd03722e6e75088263f8c63886dad6b7
Minakshi Purohit <75977092+minakshi693@users.noreply.github.com>
2026-03-29 00:10:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.post("/analyze", upload.single("file"), async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
